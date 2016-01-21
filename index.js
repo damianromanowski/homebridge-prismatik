619ea111a459303807b8fe6673fd05d6d56db640
Damian Romanowski <damian.m.romanowski@gmail.com>
2016-01-20 19:06:09 -0500

    Refactored indentation and code stanzas.

--- index.js
+++ index.js
@@ -1,235 +1,271 @@
-var color          = require('color');
-var prismatik      = require("./lib/prismatik-client");
+var color     = require('color');
+var prismatik = require("./lib/prismatik-client");
 
 module.exports = function (homebridge)
 {
-  if(typeof homebridge !== "undefined")
-  {
-    Service         = homebridge.hap.Service;
-    Characteristic  = homebridge.hap.Characteristic;
-    Accessory       = homebridge.hap.Accessory;
+    if(typeof homebridge !== "undefined")
+    {
+        console.log("Prismatik initializing");
 
-    console.log("Prismatik initializing");
-  }
+        Service         = homebridge.hap.Service;
+        Characteristic  = homebridge.hap.Characteristic;
+        Accessory       = homebridge.hap.Accessory;
 
-  homebridge.registerAccessory("homebridge-prismatik", "Prismatik", PrismatikAccessory);
+        homebridge.registerAccessory("homebridge-prismatik", "Prismatik", PrismatikAccessory);
+    }
 }
 
-function PrismatikAccessory(log, config) {
-  this.log    = log;
+function PrismatikAccessory(log, config)
+{
+    this.log    = log;
 
-  this.host   = config['host'];
-  this.port   = config['port'];
-  this.apikey = config["apikey"];
+    this.host   = config['host'];
+    this.port   = config['port'];
+    this.apikey = config["apikey"];
 
-  this.hue = 0;
-  this.sat = 100;
-  this.bri = 50;
+    this.hue    = 0;
+    this.sat    = 100;
+    this.bri    = 50;
 }
 
-PrismatikAccessory.prototype = {
+PrismatikAccessory.prototype =
+{
+    getPowerState: function(callback)
+    {
+        var options = {
+            host: this.host,
+            port: this.port,
+            apikey: this.apikey
+        };
 
-  getPowerState: function(callback) {
+        prismatik.connect(function(isConnected)
+        {
+            if (isConnected)
+            {
+                prismatik.getStatus(function(power)
+                {
+                    this.log("Getting power status: %s", power);
+                    callback(null, power);
 
-    var options = {
-       host: this.host,
-       port: this.port,
-       apikey: this.apikey
-    };
-
-    prismatik.connect(function(isConnected) {
-      if (isConnected) {
-        prismatik.getStatus(function(power) {
-          this.log("Getting power status: %s", power);
-          callback(null, power);
-
-          prismatik.disconnect();
-        }.bind(this));
-      } else {
-        this.log("Failed to connect to Prismatik");
-        callback(null, false);
-      }
-    }.bind(this), options);
-
-
-  },
-
-  setPowerState: function(powerOn, callback) {
-
-    var options = {
-       host: this.host,
-       port: this.port,
-       apikey: this.apikey
-    };
-
-    prismatik.connect(function(isConnected) {
-       if (isConnected) {
-          prismatik.lock(function(success) {
-             if (success) {
-                if (powerOn) {
-                  prismatik.turnOn(function() {
-                    this.log("Turning on!");
-                    callback();
-                  }.bind(this));
-                } else {
-                  prismatik.turnOff(function() {
-                    this.log("Turning off!");
-                    callback();
-                  }.bind(this));
-                }
-             } else {
-                this.log("Could not lock, something else has already connected.");
-                prismatik.disconnect();
-                callback();
-             }
-          }.bind(this));
-       } else {
-          this.log("Failed to connect to Prismatik");
-          callback();
-       }
-    }.bind(this), options);
-
-
-
-    
-  },
-
-  setBrightness: function(level, callback) {
-    
-    this.log("Setting brightness to %s", level);
-
-    var options = {
-       host: this.host,
-       port: this.port,
-       apikey: this.apikey
-    };
-
-    prismatik.connect(function(isConnected) {
-       if (isConnected) {
-          prismatik.lock(function(success) {
-             if (success) {
-                prismatik.setBrightness(level, function() {
-                  callback();
+                    prismatik.disconnect();
                 }.bind(this));
-             } else {
-                this.log("Could not lock, something else has already connected.");
-                prismatik.disconnect();
-                callback();
-             }
-          }.bind(this));
-       } else {
-          this.log("Failed to connect to Prismatik");
-          callback();
-       }
-    }.bind(this), options);
+            }
+            else
+            {
+                this.log("Failed to connect to Prismatik");
+                callback(null, false);
+            }
+        }.bind(this), options);
+    },
 
+    setPowerState: function(powerOn, callback)
+    {
+        var options =
+        {
+            host: this.host,
+            port: this.port,
+            apikey: this.apikey
+        };
 
-  },
-
-  setHue: function(hue, callback) {
-
-    this.log("Setting hue to %s", hue);
-    this.hue = hue;
-
-    var hsl = color({h: Math.round(this.hue), s: Math.round(this.sat), l: Math.round(this.bri)});
-
-    var options = {
-       host: this.host,
-       port: this.port,
-       apikey: this.apikey
-    };
-
-    prismatik.connect(function(isConnected) {
-       if (isConnected) {
-          prismatik.lock(function(success) {
-             if (success) {
-                prismatik.setColorToAll(hsl.red(), hsl.green(), hsl.blue(), function() {
-                  callback();
+        prismatik.connect(function(isConnected)
+        {
+            if (isConnected)
+            {
+                prismatik.lock(function(success)
+                {
+                    if (success)
+                    {
+                        if (powerOn)
+                        {
+                            prismatik.turnOn(function()
+                            {
+                                this.log("Turning on!");
+                                callback();
+                            }.bind(this));
+                        }
+                        else
+                        {
+                            prismatik.turnOff(function()
+                            {
+                                this.log("Turning off!");
+                                callback();
+                            }.bind(this));
+                        }
+                    }
+                    else
+                    {
+                        this.log("Could not lock, something else has already connected.");
+                        prismatik.disconnect();
+                        callback();
+                    }
                 }.bind(this));
-             } else {
-                this.log("Could not lock, something else has already connected.");
-                prismatik.disconnect();
+            }
+            else
+            {
+                this.log("Failed to connect to Prismatik");
                 callback();
-             }
-          }.bind(this));
-       } else {
-          this.log("Failed to connect to Prismatik");
-          callback();
-       }
-    }.bind(this), options);
+            }
+        }.bind(this), options);
+    },
 
-  },
+    setBrightness: function(level, callback)
+    {
+        this.log("Setting brightness to %s", level);
 
-  setSaturation: function(saturation, callback) {
- 
-    this.log("Setting saturation to %s", saturation);
-    this.sat = saturation;
+        var options =
+        {
+            host: this.host,
+            port: this.port,
+            apikey: this.apikey
+        };
 
-    var hsl = color({h: Math.round(this.hue), s: Math.round(this.sat), l: Math.round(this.bri)});
-
-    var options = {
-       host: this.host,
-       port: this.port,
-       apikey: this.apikey
-    };
-
-    prismatik.connect(function(isConnected) {
-       if (isConnected) {
-          prismatik.lock(function(success) {
-             if (success) {
-                prismatik.setColorToAll(hsl.red(), hsl.green(), hsl.blue(), function() {
-                  callback();
+        prismatik.connect(function(isConnected)
+        {
+            if (isConnected)
+            {
+                prismatik.lock(function(success)
+                {
+                    if (success)
+                    {
+                        prismatik.setBrightness(level, function()
+                        {
+                            callback();
+                        }.bind(this));
+                    }
+                    else
+                    {
+                        this.log("Could not lock, something else has already connected.");
+                        prismatik.disconnect();
+                        callback();
+                    }
                 }.bind(this));
-             } else {
-                this.log("Could not lock, something else has already connected.");
-                prismatik.disconnect();
+            }
+            else
+            {
+                this.log("Failed to connect to Prismatik");
                 callback();
-             }
-          }.bind(this));
-       } else {
-          this.log("Failed to connect to Prismatik");
-          callback();
-       }
-    }.bind(this), options);
+            }
+        }.bind(this), options);
+    },
 
-  },
+    setHue: function(hue, callback)
+    {
+        this.log("Setting hue to %s", hue);
+        this.hue = hue;
 
-  
-  identify: function(callback) {
-    this.log("Identify requested!");
-    callback(); // success
-  },
-  
-  getServices: function() {
+        var hsl = color({h: Math.round(this.hue), s: Math.round(this.sat), l: Math.round(this.bri)});
 
-    // you can OPTIONALLY create an information service if you wish to override
-    // the default values for things like serial number, model, etc.
-    var informationService = new Service.AccessoryInformation();
-    
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, "Prismatik Manufacturer")
-      .setCharacteristic(Characteristic.Model, "Prismatik Model")
-      .setCharacteristic(Characteristic.SerialNumber, "Prismatik Serial Number");
-    
-    var lightbulbService = new Service.Lightbulb();
-    
-    lightbulbService
-      .getCharacteristic(Characteristic.On)
-      .on('set', this.setPowerState.bind(this))
-      .on('get', this.getPowerState.bind(this));
-    
-    lightbulbService
-      .addCharacteristic(new Characteristic.Brightness())
-      .on('set', this.setBrightness.bind(this));
+        var options =
+        {
+            host: this.host,
+            port: this.port,
+            apikey: this.apikey
+        };
 
-    lightbulbService
-      .addCharacteristic(new Characteristic.Hue())
-      .on('set', this.setHue.bind(this));
+        prismatik.connect(function(isConnected)
+        {
+            if (isConnected)
+            {
+                prismatik.lock(function(success)
+                {
+                    if (success)
+                    {
+                        prismatik.setColorToAll(hsl.red(), hsl.green(), hsl.blue(), function()
+                        {
+                            callback();
+                        }.bind(this));
+                    }
+                    else
+                    {
+                        this.log("Could not lock, something else has already connected.");
+                        prismatik.disconnect();
+                        callback();
+                    }
+                }.bind(this));
+            }
+            else
+            {
+                this.log("Failed to connect to Prismatik");
+                callback();
+            }
+        }.bind(this), options);
+    },
 
-    lightbulbService
-      .addCharacteristic(new Characteristic.Saturation())
-      .on('set', this.setSaturation.bind(this));
-        
-    return [informationService, lightbulbService];
-  }
+    setSaturation: function(saturation, callback)
+    {
+        this.log("Setting saturation to %s", saturation);
+        this.sat = saturation;
+
+        var hsl = color({h: Math.round(this.hue), s: Math.round(this.sat), l: Math.round(this.bri)});
+
+        var options =
+        {
+            host: this.host,
+            port: this.port,
+            apikey: this.apikey
+        };
+
+        prismatik.connect(function(isConnected)
+        {
+            if (isConnected)
+            {
+                prismatik.lock(function(success)
+                {
+                    if (success)
+                    {
+                        prismatik.setColorToAll(hsl.red(), hsl.green(), hsl.blue(), function()
+                        {
+                            callback();
+                        }.bind(this));
+                    }
+                    else
+                    {
+                        this.log("Could not lock, something else has already connected.");
+                        prismatik.disconnect();
+                        callback();
+                    }
+                }.bind(this));
+            } else {
+                this.log("Failed to connect to Prismatik");
+                callback();
+            }
+        }.bind(this), options);
+    },
+
+    identify: function(callback)
+    {
+        this.log("Identify requested!");
+        callback();
+    },
+
+    getServices: function()
+    {
+        var informationService = new Service.AccessoryInformation();
+
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "Prismatik Manufacturer")
+            .setCharacteristic(Characteristic.Model, "Prismatik Model")
+            .setCharacteristic(Characteristic.SerialNumber, "Prismatik Serial Number");
+
+        var lightbulbService = new Service.Lightbulb();
+
+        lightbulbService
+            .getCharacteristic(Characteristic.On)
+            .on('set', this.setPowerState.bind(this))
+            .on('get', this.getPowerState.bind(this));
+
+        lightbulbService
+            .addCharacteristic(new Characteristic.Brightness())
+            .on('set', this.setBrightness.bind(this));
+
+        lightbulbService
+            .addCharacteristic(new Characteristic.Hue())
+            .on('set', this.setHue.bind(this));
+
+        lightbulbService
+            .addCharacteristic(new Characteristic.Saturation())
+            .on('set', this.setSaturation.bind(this));
+
+        return [informationService, lightbulbService];
+    }
 };
